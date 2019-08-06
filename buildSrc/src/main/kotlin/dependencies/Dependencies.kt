f21b94ea76689235353f394ad280b8b4f2844922
SatoShun <shun.sato1@gmail.com>
2019-08-06 04:37:58 +0000

    update

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -4,10 +4,10 @@ const val COMPILE_SDK = 28
 const val MIN_SDK = 25
 const val TARGET_SDK = 28
 
-private const val VKOTLIN = "1.3.40"
+private const val VKOTLIN = "1.3.41"
 private const val VNAVIGATION = "1.0.0"
 
-const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.5.0-beta05"
+const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.5.0-rc02"
 const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VKOTLIN"
 const val SAFEARGS_PLUGIN = "android.arch.navigation:navigation-safe-args-gradle-plugin:$VNAVIGATION"
 const val VERSIONS_PLUGIN = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
