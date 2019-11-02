f6855f27d1dda9818cd21b64f122de2117dc13a0
SatoShun <shun.sato1@gmail.com>
2019-11-02 22:29:08 +0000

    update

--- buildSrc/src/main/kotlin/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies/Dependencies.kt
@@ -1,28 +1,28 @@
 package dependencies
 
-const val COMPILE_SDK = 28
+const val COMPILE_SDK = 29
 const val MIN_SDK = 25
-const val TARGET_SDK = 28
+const val TARGET_SDK = 29
 
-private const val VKOTLIN = "1.3.41"
+private const val VKOTLIN = "1.3.50"
 private const val VNAVIGATION = "1.0.0"
 
-const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.5.0-rc02"
+const val ANDROID_PLUGIN = "com.android.tools.build:gradle:3.5.1"
 const val KOTLIN_PLUGIN = "org.jetbrains.kotlin:kotlin-gradle-plugin:$VKOTLIN"
 const val SAFEARGS_PLUGIN = "android.arch.navigation:navigation-safe-args-gradle-plugin:$VNAVIGATION"
 const val VERSIONS_PLUGIN = "com.github.ben-manes:gradle-versions-plugin:0.20.0"
 
-private const val VCOUROUTINE = "1.1.1"
+private const val VCOUROUTINE = "1.3.2"
 const val KOTLIN = "org.jetbrains.kotlin:kotlin-stdlib:$VKOTLIN"
 const val COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$VCOUROUTINE"
 const val UI_COROUTINE = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$VCOUROUTINE"
 
-const val KTX = "androidx.core:core-ktx:1.1.0-alpha04"
-const val ACTIVITYX = "androidx.activity:activity-ktx:1.0.0-alpha08"
-const val FRAGMENTX = "androidx.fragment:fragment-ktx:1.1.0-alpha09"
+const val KTX = "androidx.core:core-ktx:1.1.0"
+const val ACTIVITYX = "androidx.activity:activity-ktx:1.0.0"
+const val FRAGMENTX = "androidx.fragment:fragment-ktx:1.1.0"
 
-const val APPCOMPAT = "androidx.appcompat:appcompat:1.1.0-alpha02"
-const val RECYCLERVIEW = "androidx.recyclerview:recyclerview:1.1.0-alpha02"
+const val APPCOMPAT = "androidx.appcompat:appcompat:1.1.0"
+const val RECYCLERVIEW = "androidx.recyclerview:recyclerview:1.0.0"
 const val CONSTRAINTLAYOUT = "androidx.constraintlayout:constraintlayout:2.0.0-alpha5"
 const val CARDVIEW = "androidx.cardview:cardview:1.0.0"
 
@@ -41,10 +41,10 @@ const val ROOM_COMMON = "androidx.room:room-common:$VROOM"
 const val ROOM_RUNTIME = "androidx.room:room-runtime:$VROOM"
 const val ROOM_COMPILER = "androidx.room:room-compiler:$VROOM"
 
-const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0-alpha01"
-const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata-ktx:2.1.0-alpha03"
+const val VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:2.1.0"
+const val LIVEDATA = "androidx.lifecycle:lifecycle-livedata-ktx:2.1.0"
 const val LIFECYCLE_COMPILER = "androidx.lifecycle:lifecycle-compiler:2.0.0"
-const val LIFECYCLE_EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:2.1.0-alpha03"
+const val LIFECYCLE_EXTENSIONS = "androidx.lifecycle:lifecycle-extensions:2.1.0"
 
 const val BIOMETRIC = "androidx.biometric:biometric:1.0.0-alpha03"
 
@@ -52,9 +52,9 @@ const val EMOJI = "androidx.emoji:emoji:1.0.0"
 const val EMOJI_COMPAT = "androidx.emoji:emoji-appcompat:1.0.0"
 const val EMOJI_BUNDLED = "androidx.emoji:emoji-bundled:1.0.0"
 
-const val SAVEDSTATE = "androidx.savedstate:savedstate:1.0.0-beta01"
+const val SAVEDSTATE = "androidx.savedstate:savedstate:1.0.0"
 const val SAVEDSTATE_BUNDLE = "androidx.savedstate:savedstate-bundle:1.0.0-alpha01"
-const val SAVEDSTATE_VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-savedstate:1.0.0-alpha01"
+const val SAVEDSTATE_VIEWMODEL = "androidx.lifecycle:lifecycle-viewmodel-savedstate:1.0.0-rc01"
 
 const val VIEWPAGER2 = "androidx.viewpager2:viewpager2:1.0.0-alpha04"
 
